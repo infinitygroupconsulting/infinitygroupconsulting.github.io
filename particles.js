d82f2db2eb2e4e45072ce496404711e197685d8c
infinitygroupconsulting <infinitygroupconsulting@gmail.com>
2025-06-17 23:41:49 +0800

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    particlesJS('particles-js', {
+        "particles": {
+            "number": { "value": 40, "density": { "enable": true, "value_area": 800 } },
+            "color": { "value": "#C0A062" },
+            "shape": { "type": "circle" },
+            "opacity": { "value": 0.5, "random": true },
+            "size": { "value": 3, "random": true },
+            "line_linked": { "enable": false },
+            "move": {
+                "enable": true,
+                "speed": 1,
+                "direction": "none",
+                "random": true,
+                "straight": false,
+                "out_mode": "out"
+            }
+        },
+        "interactivity": {
+            "events": {
+                "onhover": { "enable": true, "mode": "repulse" }
+            }
+        }
+    });
+});
